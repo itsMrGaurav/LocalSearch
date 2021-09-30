5db0230e6ab884a93e641577bf7b90e76b33ead7
itsKiranay <itskiranay@gmail.com>
2021-09-30 23:12:51 +0530

    skeleton ready

--- app/app.py
+++ app/app.py
@@ -15,19 +15,19 @@ Builder.load_file('design.kv')
 class CustomPopup(Popup):
 	def __init__(self, **kwargs):
 		super(Popup, self).__init__(**kwargs)
-		self.content_0 = ""
-		self.content_1 = ""
-		self.content_2 = ""
+		self.dirname = ""
+		self.filename = ""
+		self.info = ""
 
 	def _open(self, main):
 		self.open()
 		self.main_obj = main
 
 	def _validate(self):
-		self.content_0 = self.ids.content_layer0.text
-		self.content_1 = self.ids.content_layer1.text
-		self.content_2 = self.ids.content_layer2.text
-		if (not self.content_0 or not self.content_1 or not self.content_2):
+		self.dirname = self.ids._chapter.text.strip().lower()
+		self.filename = self.ids._topic.text.strip().lower()
+		self.info = self.ids._info.text.strip()
+		if (not self.dirname or not self.filename or not self.info):
 			err_btn = Button(text = "Input fields cannot be empty!!")
 			err_pop = Popup(
 				title = "ERROR",
@@ -40,9 +40,9 @@ class CustomPopup(Popup):
 			err_pop.open()
 			return
 		else:
-			pat = re.compile("^[a-zA-Z]{3,10}$")
-			if (not pat.match(self.content_0) or not pat.match(self.content_1)):
-				self.ids.no_validate.text = "Invalid input in \n layer0 or layer1"
+			pat = re.compile("^[a-z]{3,10}$")
+			if (not pat.match(self.dirname) or not pat.match(self.filename)):
+				self.ids.no_validate.text = "Invalid input for \n Chapter or Topic"
 				return
 
 		self.ids.no_validate.text = ""
@@ -51,50 +51,52 @@ class CustomPopup(Popup):
 
 	def _add(self):
 		self.dismiss()
-		
-		btn =  Button(
-			text = self.content_0,
-			size_hint_y = None,
-			height = 50,
-			on_press = self.main_obj.pressed_layer0
-		)
-		self.main_obj.ids.layer0.add_widget(btn, index=1)
-		self.main_obj.chapters.append(self.content_0)
-
-		self.ids.content_layer0.text = ""
-		self.ids.content_layer1.text = ""
-		self.ids.content_layer2.text = ""
+		self.ids._chapter.text = ""
+		self.ids._topic.text = ""
+		self.ids._info.text = ""
 		self.ids.add_button.disabled= True
 		self.data_storage()
 
 
 	def data_storage(self):
-		
+
 		#list new directory name into dirs list
 		dir_exst = False 
-		with open("../data/dirs.txt","a+") as fp:
+		with open("../data/dirs.txt","r+") as fp:
 			for dirt in fp.readlines():
-				if dirt[:-1].lower() == self.content_0.lower():
+				if dirt[:-1] == self.dirname:
 					dir_exst = True
 			if not dir_exst:
-				os.system(f"mkdir ../data/{self.content_0}")
-				fp.write(self.content_0+'\n')
+				os.system(f"mkdir ../data/{self.dirname}")
+				fp.write(self.dirname+'\n')
+				btn = Button(
+					text = self.dirname.upper(),
+					size_hint_y = None,
+					height = 50,
+					on_press = self.main_obj.pressed_chapter
+				)
+				self.main_obj.ids.chapter_.add_widget(btn, index=1)
 		fp.close()
 		
 		# list new filename into files list
 		file_exst = False
-		with open(f"../data/{self.content_0}/files.txt","a+") as fp:
-			for file in fp.readlines():
-				if file[:-1].lower() == self.content_1.lower():
-					file_exst = True
-			if not file_exst:
-				fp.write(self.content_1+'\n')
-		fp.close()
+		try:
+			with open(f"../data/{self.dirname}/files.txt","r+") as fp:
+				for file in fp.readlines():
+					if file[:-1] == self.filename:
+						file_exst = True
+				if not file_exst:
+					fp.write(self.filename+'\n')
+			fp.close()
+		except FileNotFoundError:
+			with open(f"../data/{self.dirname}/files.txt","w") as fp:
+				fp.write(self.filename+'\n')
+			fp.close()
 
-		#Create a new file and add data to it
-		# with open(f"../data/{self.content_0}/{self.content_1}.txt","a") as fp:
-		# 	fp.write(self.content_2+'\n')
-		# fp.close()
+		# Create a new file and add data to it
+		with open(f"../data/{self.dirname}/{self.filename}.txt","a") as fp:
+			fp.write(self.info+'\n')
+		fp.close()
 
 
 
@@ -104,22 +106,20 @@ class MyLayout(Widget):
 
 	def __init__(self,**kwargs):
 		super(MyLayout, self).__init__(**kwargs)
-		self.chapters = []
 		self.add_chapters()
 		self.pop = CustomPopup()
 
 	def add_chapters(self):
 		try:
 			with open("../data/dirs.txt",'r') as fp:
-				for chapter in fp.readlines():
+				for line in fp.readlines():
 					btn =  Button(
-						text = chapter[:-1],
+						text = line[:-1].upper(),
 						size_hint_y = None,
 						height = 50,
-						on_press = self.pressed_layer0
+						on_press = self.pressed_chapter
 					)
-					self.ids.layer0.add_widget(btn, index=1)
-					self.chapters.append(chapter)
+					self.ids.chapter_.add_widget(btn, index=1)
 			fp.close()
 
 		except FileNotFoundError:
@@ -128,35 +128,34 @@ class MyLayout(Widget):
 	def add_item_layer0(self):
 		dataset = self.pop._open(self)
 
-	def pressed_layer0(self, instance):
-		dirname = instance.text
+	def pressed_chapter(self, instance):
+		self.dirname = instance.text.lower()
 
-		self.ids.layer1.clear_widgets(children = None)
+		self.ids.topic_.clear_widgets(children = None)
 
-		self.ids.layer1.add_widget(Button(
-			text = "+ADD TOPIC",
-			size_hint_y=  None,
-			height = 50,
-		))
-		with open(f"../data/{dirname}/files.txt",'r') as fp:
+		with open(f"../data/{self.dirname}/files.txt",'r') as fp:
 			for topic in fp.readlines():
 				btn =  Button(
-					text = topic[:-1],
+					text = topic[:-1].upper(),
 					size_hint_y = None,
 					height = 50,
-					on_press = self.pressed_layer1
+					on_press = self.pressed_topic
 				)
-				self.ids.layer1.add_widget(btn, index=1)
+				self.ids.topic_.add_widget(btn, index=1)
 
 
-	def pressed_layer1(self, instance):
-		pass
+	def pressed_topic(self, instance):
+		filename = instance.text.lower()
+		self.ids.info_.text = ""
+		with open(f"../data/{self.dirname}/{filename}.txt") as fp:
+			self.ids.info_.text = ''.join(fp.readlines())
+		fp.close()
+		self.dirname = ""
+		
 
 class LocalSearchApp(App):
 	def build(self):
 		return MyLayout()
-		
-
 
 if __name__ == "__main__":
 	app = LocalSearchApp()
